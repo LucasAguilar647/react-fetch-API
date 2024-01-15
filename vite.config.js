9ae59a39a891f73bb6da91b7bced42411c676bac
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-01-14 22:22:24 -0300

    agregue el base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://LucasAguilar647.github.io/react-fetch-API',
 })
